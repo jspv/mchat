ce5cc0d79fba47d896d648a6b3cada316e96d1bd
jspv <jspvgithub@twinleaf.xyz>
2024-12-21 18:04:19 -0500

    Fixed colors, improved serve capabilites

--- serve.py
+++ serve.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
+import click
 from textual_serve.server import Server
 
-server = Server("mchat")
-server.serve()
+
+@click.command()
+@click.option("--command", default="python -m mchat.mchat", help="Command to run")
+@click.option("--port", default=5500, help="Port to serve on")
+def serve(command, port):
+    server = Server(command=command, port=port)
+    server.serve()
+
+
+if __name__ == "__main__":
+    serve()
